8e85b57de7072b89cceb65bce83ff8c16b56e0dd
ImdaduRohman23 <ir.imdadurohman@gmail.com>
2022-12-29 18:31:00 +0700

    fetch API

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,16 +1,35 @@
 import { Box, Stack, Typography } from '@mui/material'
-import React from 'react'
-import SideBar from './SideBar'
+import React, { useEffect, useState } from 'react';
+import SideBar from './SideBar';
+
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+import Videos from './Videos';
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState('New')
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then(data => setVideos(data.items))
+  }, [setSelectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: {sx: "column", md: "row"}}}>
       <Box sx={{height: {sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: {sx: 0, md: 2}}}>
-        <SideBar />
+        <SideBar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}/>
 
         <Typography className="copyright" variant="body2" sx={{mt: 1.5, color: '#fff'}}>
           Copyright 2022 Imdadu Rohman
         </Typography>
+      </Box>
+
+      <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2}}>
+        <Typography variant='h4' fontWeight='bold' mb={2} sx={{color: 'white'}}>
+          {selectedCategory} <span style={{color: '#f31503'}}>Videos</span>
+        </Typography>
+
+        <Videos videos={videos} />
       </Box>
 
     </Stack>
---
+++ src/components/Videos.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Videos = ({videos}) => {
+  console.log('vidoes', videos)
+
+  return (
+    <div>
+      <h1>Videos</h1>
+    </div>
+  )
+}
+
+export default Videos
